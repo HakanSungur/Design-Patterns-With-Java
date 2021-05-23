15a8ccb324cf17f58693e97b7a04186187db4b42
hakansungur <hakansungur@gmail.com>
2021-05-24 02:47:44 +0300

    İlk Dosya Atımı

--- src/intro/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        String internetSubeButonu= "HalkBank";
-        System.out.println(internetSubeButonu);
-    }
-}
--- src/untitled1/src/Demo.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.*;
-
-public class Demo {
-    private JRadioButton radioButton1;
-}
